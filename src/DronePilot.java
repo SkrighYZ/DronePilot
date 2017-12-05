e6076ac764361d674fd7bf0a8c3be155f036a51a
Yipeng Zhang <31830189+SkrighYZ@users.noreply.github.com>
2017-12-04 21:13:14 -0500

    Update DronePilot.java

--- src/DronePilot.java
+++ src/DronePilot.java
@@ -3,7 +3,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -349,6 +348,7 @@ public class DronePilot extends JPanel implements KeyListener{
 		
 		if(rc.x >= 480 && rc.x < 720) {
 			if((rc.y + 100) >= 500){
+				bg.triggered = true;
 				return true;
 			}
 		}
@@ -380,3 +380,4 @@ public class DronePilot extends JPanel implements KeyListener{
 		f.setVisible(true);
 	}
 }
+
