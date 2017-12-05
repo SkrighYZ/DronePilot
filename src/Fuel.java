4e8b0cbe7225030b84758c414a388a8ad4e3788f
Yipeng Zhang <31830189+SkrighYZ@users.noreply.github.com>
2017-12-04 23:20:20 -0500

    Update Fuel.java

--- src/Fuel.java
+++ src/Fuel.java
@@ -2,11 +2,11 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.Timer;
 
+/**
+ * Class that draws the fuel bar of the rocket.
+ */
 public class Fuel extends JPanel implements ActionListener {
 
 	protected int x = 1200;
@@ -15,10 +15,18 @@ public class Fuel extends JPanel implements ActionListener {
 	protected boolean dead = false;
 	protected static double TIME_DELAY;
 	
+	/**
+	 * Constructor.
+	 * @param TIME_DELAY in seconds.
+	 */
 	public Fuel(double TIME_DELAY) {
 		this.TIME_DELAY = TIME_DELAY;
 	}
 
+	/**
+	 * Decreases the length of the fuel bar.
+	 * @param e ActionEvent.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		length -= 1;
@@ -27,6 +35,10 @@ public class Fuel extends JPanel implements ActionListener {
 		}
 	}
 	
+	/**
+	 * Draws the fuel bar.
+	 * @param g Graphics object.
+	 */
 	public void draw(Graphics g) {
 		super.paintComponent(g);
 		g.setColor(Color.GREEN);
