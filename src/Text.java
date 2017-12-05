88c063f0c62f6d11203cef3f80f5cc02aadc74fb
Yipeng Zhang <31830189+SkrighYZ@users.noreply.github.com>
2017-12-04 23:15:23 -0500

    Update Text.java

--- src/Text.java
+++ src/Text.java
@@ -2,21 +2,32 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+/**
+ * Class that draws the end game message.
+ */
 public class Text extends JPanel implements ActionListener{
 
 	protected int x1, y1, x2, y2;
 	protected boolean win;
 	protected int font = 0;
 	
+	/**
+	 * Constructor.
+	 * @param x coordinate of the baseline of the leftmost character of the texts.
+	 * @param y coordinate of the baseline of the leftmost character of the texts.
+	 */
 	public Text(int x, int y) {
 		this.x1 = x2 = x;
 		this.y1 = y2 = y;
 		win = false;
 	}
 
+	/**
+	 * Increases the size and leftmost point of the texts.
+	 * @param e ActionEvent.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		font += 10;
@@ -25,6 +36,10 @@ public class Text extends JPanel implements ActionListener{
 		y2 += 5;
 	}
 
+	/**
+	 * Draws the message.
+	 * @param g Graphics object.
+	 */
 	public void draw(Graphics g) {
 		
 		super.paintComponent(g);
