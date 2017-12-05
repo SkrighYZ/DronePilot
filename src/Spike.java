bc972117831532d12eded106e43b05db1ac3e5b5
Yipeng Zhang <31830189+SkrighYZ@users.noreply.github.com>
2017-12-04 23:32:31 -0500

    Update Spike.java

--- src/Spike.java
+++ src/Spike.java
@@ -5,26 +5,45 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+/**
+ * Class that draws the first spike trap.
+ */
 public class Spike extends JPanel implements ActionListener {
 
-	protected int num = 8;
-	protected int length = 40;
-	protected int height = 80;
-	protected int rtXPos = 920;
-	protected int upYPos = 200;
-	protected boolean dead = false;
-	
+	protected int num;
+	protected int length;
+	protected int height;
+	protected int rtXPos;
+	protected int upYPos;
+	protected boolean dead;
 	protected static double TIME_DELAY;
 	
+	/**
+	 * Constructor.
+	 * @param TIME_DELAY in seconds.
+	 */
 	public Spike(double TIME_DELAY) {
+		num = 8;  	// Number of spikes times 2
+		length = 40;
+		height = 80;
+		rtXPos = 920;
+		upYPos = 200;
+		dead = false;
 		this.TIME_DELAY = TIME_DELAY;
 	}
 
+	/**
+	 * The spikes moving towards the left.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		rtXPos -= 15;
 	}
 	
+	/**
+	 * Draws the spikes
+	 * @param g Graphics object.
+	 */
 	public void draw(Graphics g) {
 		super.paintComponent(g);
 		g.setColor(Color.BLACK);
@@ -42,7 +61,6 @@ public class Spike extends JPanel implements ActionListener {
 		
 	}
 
-	
 	public boolean isDead() {
 		return dead;
 	}
